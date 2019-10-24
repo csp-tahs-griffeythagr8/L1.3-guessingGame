278bab3c48fec14bd577dc77bf48934e6db850b6
RW3110 <RW3110@k12.sd.us>
2019-10-24 09:11:48 -0500

    Update Guessing Game.py

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,7 +1,7 @@
 """
 Guessing_Game.py
-Programmer(s):
-This is a guessing game designed by students of the TAHS PLTW CSP class. 
+Programmer(s):Reece Westhoff and Ethan Stainbrook
+This is a guessing game designed by students of the TAHS PLTW CSP class.
 """
 
-#Activity 1.3.2 - Create all variables and functions needed to run this game. 
+#Activity 1.3.2 - Create all variables and functions needed to run this game.
